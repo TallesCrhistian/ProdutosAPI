1c362461f554dff841e7023f2ef543c3361c0f6f
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-08-03 16:35:43 -0300

    Adicionar arquivos de projeto.

---
+++ ProdutosAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutosAPI.Models;
+
+namespace ProdutosAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }   
+        public DbSet<Endereco> Enderecoes { get; set; }
+
+    }
+}
---
+++ ProdutosAPI/Models/Endereco.cs
@@ -0,0 +1,15 @@
+﻿namespace ProdutosAPI.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+    }
+}
